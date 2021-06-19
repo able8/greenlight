f4191dd9af725dabe094e1526ef11e560b8ceb8d
able.lv <able8@me.com>
2021-06-19 23:38:23 +0800

    Chapter 3.2 JSON Encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a fixed-format JSON response from a string.
-	js := `{"status":"available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// js := `{"status":"available", "environment": %q, "version": %q}`
+	// js = fmt.Sprintf(js, app.config.env, version)
+
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// Pass the map to the json.Marshal() function.
+	// This returns a []byte slice containing the encoded JSON.
+	// If there was an error, we log it and send the client a generic error message.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicety to make it easier to view in terminal application.
+	js = append(js, '\n')
 
 	// Set the "Content-Type: application/json" header on the response.
 	// If you forget to this, Go will default to sending
