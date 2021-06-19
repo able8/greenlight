2d52845f224657a25682bd1987bee828d90ce05f
able.lv <able8@me.com>
2021-06-19 22:21:53 +0800

    Creating a helper to read ID parameters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // For the "POST /v1/movies" endpoint.
@@ -16,12 +13,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // For the "GET /v1/movies/:id" endpoints.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
-	// When httprouter is parsing a request, any interpolated URL parameters will be
-	// stored in the request context. We can use the ParamsFromContext() function to
-	// retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
+	id, err := app.readIDParam(r)
 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
