9d5a111782afe4c23b2f2bd9973b3ad0689a0a64
able.lv <able8@me.com>
2021-06-25 19:13:33 +0800

    Adding Validation Checks

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/able8/greenlight/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -56,4 +57,39 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "Email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "Password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	// Call the standlone ValidateEmail() helper.
+	ValidateEmail(v, user.Email)
+
+	// If the plaintext password is not nil, call the standalone
+	// ValidatePasswordPlaintext() helper.
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	// If the password hash is ever nil, this will be due to a logic error in our
+	// codebase (probably because we forgot to set a password for the user). It's a
+	// userful sanity check to include here, but it's not a problem with the data
+	// provided by the client. So rather than adding an error to the validation map
+	// we raise a panic instead.
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
