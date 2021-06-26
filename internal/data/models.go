4ea6fdf844337ad7d9aaab8372e5c47f92d20e13
able.lv <able8@me.com>
2021-06-26 18:36:36 +0800

    17.3. Setting up the Permissions Model

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,17 +13,19 @@ var (
 // Create a Models struct which wraps the MovieModel.
 // We'll add other models to this, like a UserModel and PermissionModel.
 type Models struct {
-	Movies MovieModel
-	Users  UserModel
-	Tokens TokenModel
+	Movies      MovieModel
+	Users       UserModel
+	Tokens      TokenModel
+	Permissions PermissionModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct
 // containing the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
-		Tokens: TokenModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Users:       UserModel{DB: db}, // Initialize a new UserModel instance.
+		Tokens:      TokenModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
